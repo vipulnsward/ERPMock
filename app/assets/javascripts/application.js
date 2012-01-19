0cdf5f4e19d40bcccf20255a48e55eeb421811ec
Vipul A M <vipulnsward@gmail.com>
2012-01-19 07:42:20 -0800

    Initial Commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,31 @@
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application.js
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+
+$(document).ready(function() {
+    // put all your jQuery goodness in here.
+		var icons = {
+			header: "ui-icon-circle-arrow-e",
+			headerSelected: "ui-icon-circle-arrow-s"
+		};
+       
+        $("#accordion").accordion({
+	event: "mouseover",
+	icons : icons
+	});
+
+	 $("#accordion-left").accordion({
+        event: "mouseover",
+        icons : icons
+        });
+
+
+
+});
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyMock::Application.config.secret_token = '2440778dcf12c81ba735859d1575168f6e7742f9de17938b8e5a169047c37023cf3205d167eb03a5d082b330d5266ea3294a405d92d509982fcf9c4b3f504f8a'
